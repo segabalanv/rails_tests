15d0615966914998718da0d2854276fd59f0c145
Sergio Gabalán <segabalanv@unal.edu.co>
2016-07-17 19:22:12 +0000

    created simple app with rails and a simple css template

---
+++ app1/app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+class HomeController < ApplicationController
+  layout "application", :only => "index"
+  def index
+    @titulo = "Empresa ROR - Inicio"
+  end
+
+  def contacto
+    @titulo = "Empresa ROR - Contacto"
+  end
+
+  def localizacion
+    @titulo = "Empresa ROR - Localización"
+  end
+
+  def quienes_somos
+    @titulo = "Empresa ROR - ¿Quiénes Somos?"
+  end
+
+  def servicios
+    @titulo = "Empresa ROR - Servicios"
+  end
+end
---
+++ app1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = 'a5a83b970f01cc36835a0c209d92beb888cfc93c70d8b41bc95542cea89c1638b479381068eebdfa9302503e3a3f67fc283366d6504583c3f4492c34c6cdfd8e'
